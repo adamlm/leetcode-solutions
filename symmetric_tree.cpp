b68ec34049a1c3d793915a1780fc72be3c6ebf9d
Adam Morrissett <me@adamlm.com>
2020-04-06 22:03:29 -0400

    Add solution for symmetric tree

---
+++ symmetric_tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return helper(root, root);
+    }
+    
+    bool helper(TreeNode* node1, TreeNode* node2) {
+        if (node1 == nullptr || node2 == nullptr) {
+            return node1 == node2;
+        }
+        
+        if (node1->val != node2->val) {
+            return false;
+        }
+        
+        return helper(node1->left, node2->right) && helper(node1->right, node2->left);
+    }
+};
